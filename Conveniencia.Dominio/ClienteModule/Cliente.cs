cbb55a19fdf53f2042074d4de56b51a65ed7c935
Diconcili <eduardodiconcilicosta@yahoo.com.br>
2021-10-03 15:41:45 -0300

    classe cliente criada

---
+++ Conveniencia.Dominio/ClienteModule/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conveniencia.Dominio.ClienteModule
+{
+    public class Cliente
+    {
+        string nome;
+        string cpf;
+        string endereço;
+        string telefone;
+        string senha;
+        string email;
+        int limiteCredito;
+        string cartaoAlimentacao;
+        int saldoDevedor;
+
+    }
+}
