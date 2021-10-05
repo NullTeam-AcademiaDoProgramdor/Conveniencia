d78dfa4a11e7cd731919c936decfacf3d54acadd
Erik Borela <erikborella@gmail.com>
2021-10-04 21:04:56 -0300

    Criada configuração base para o Entity

---
+++ Conveniencia.ORM/ConvenienciaDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conveniencia.ORM
+{
+    public class ConvenienciaDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlite("Data Source=Banco.db;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ConvenienciaDbContext).Assembly);
+        }
+    }
+}
